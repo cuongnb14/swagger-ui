aefd44ada4b3437e2fb0bbd7bfa2d75c92468477
Cuong Nguyen <cuongnb14@gmail.com>
2017-06-08 18:28:17 +0700

    add ALLOWED_HOSTS = *

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,7 +14,7 @@ APPS_DIR = ROOT_DIR.path('swagger_ui')
 
 # Load operating system environment variables and then prepare to use them
 env = environ.Env()
-
+ALLOWED_HOSTS = ["*"]
 # .env file, should load only in development environment
 READ_DOT_ENV_FILE = env.bool('DJANGO_READ_DOT_ENV_FILE', default=False)
 
