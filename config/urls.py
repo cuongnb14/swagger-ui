b235e5207af192d39ad117eef069e35da85ece35
Cuong Nguyen <cuongnb14@gmail.com>
2017-06-08 18:15:52 +0700

    change home page to admin page

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^$', RedirectView.as_view(url="/admin"), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
